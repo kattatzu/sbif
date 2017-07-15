981af4ee62115aa2982fd706d7142611303282d8
Jose Eduardo Rios <jrios@digitalmeat.cl>
2017-07-15 02:41:10 -0400

    Fix return error

--- src/SBIF.php
+++ src/SBIF.php
@@ -223,7 +223,7 @@ class Sbif
 			case self::IND_IPC:
 				$value = isset($body->IPCs[0]) ? $body->IPCs[0]->Valor : 0; break;
 			case self::INF_BANK:
-				$value = isset($body->Perfiles[0]) ? $body->Perfiles[0] : null; break;
+				return isset($body->Perfiles[0]) ? $body->Perfiles[0] : null; break;
 		}
 
 		return (double) $value;
