24e22029d41b91f9eff9d3119657ad39e4fad6b6
Jose Eduardo Rios <jrios@digitalmeat.cl>
2017-07-15 02:03:46 -0400

    Add helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,40 @@
 <?php
-
 if (! function_exists('sbif_dollar'))
 {
     function sbif_dollar($date = null)
     {
-    	return "hi";
+    	return Sbif::getDollar($date);
+    }
+}
+
+if (! function_exists('sbif_euro'))
+{
+    function sbif_euro($date = null)
+    {
+    	return Sbif::getEuro($date);
+    }
+}
+
+if (! function_exists('sbif_utm'))
+{
+    function sbif_utm($date = null)
+    {
+    	return Sbif::getUTM($date);
+    }
+}
+
+if (! function_exists('sbif_uf'))
+{
+    function sbif_uf($date = null)
+    {
+    	return Sbif::getUF($date);
+    }
+}
+
+if (! function_exists('sbif_ipc'))
+{
+    function sbif_ipc($date = null)
+    {
+    	return Sbif::getIPC($date);
     }
 }
